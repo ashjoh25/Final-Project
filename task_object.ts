ad6516c443c26e07b5cda2555689762b42432399
Ashley Johnson <ajohnson100920@gmail.com>
2022-05-24 12:31:35 -0400

    started task class, slightly started main.html, added to opener css

--- task_object.ts
+++ task_object.ts
@@ -1,1 +1,15 @@
 // properties and behaviors of a Task
+class Task {
+    list : string;
+    x : number;
+    y : number;
+    
+    constructor(list : string, x: number, y : number) {
+        this.list = list;
+        this.x = x;
+        this.y = y;
+    }
+    movetask(Movex : number, Movey : number){
+
+    }
+}
