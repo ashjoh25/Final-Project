c30e6ae5e23ad1383f6ba563c38f5588c81a1af0
vrigue <vrielleguevarra@gmail.com>
2022-05-26 12:31:30 -0400

    finished css grid, removed manager

--- event_handlers.ts
+++ event_handlers.ts
@@ -1,1 +1,9 @@
 // user-input event handlers 
+
+let username = document.querySelector("name") as HTMLInputElement;
+let numberoflists = document.querySelector("lists") as HTMLInputElement;
+let namesoflists = document.querySelector("listnames") as HTMLInputElement;
+
+let user : string = username.value
+let numoflists : number = Number(numberoflists.value)
+let listnames : string = namesoflists.value
--- manager.ts
+++
@@ -1,9 +0,0 @@
-// Manages query selects and references to other files
-let username = document.querySelector("name") as HTMLInputElement;
-let numberoflists = document.querySelector("lists") as HTMLInputElement;
-let namesoflists = document.querySelector("listnames") as HTMLInputElement;
-
-let user : string = username.value
-let numoflists : number = Number(numberoflists.value)
-let listnames : string = namesoflists.value
-
